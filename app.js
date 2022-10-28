86a3976cb2984a98c4b9edadd3812ae11d6d0629
Estefania Sanchez <estefysc@gmail.com>
2022-10-28 18:03:27 -0400

    fixing server app functions to work with heroku

--- app.js
+++ app.js
@@ -14,52 +14,33 @@ if(port == null || port == "") {
     port = 5000;
 }
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static('frontend/build'));
-    app.get('*', (req, res) => {
-        req.sendFile(path.resolve(__dirname, 'frontend/build', 'index.html'));
-    })
-    app.get("/", function(req, res){
-        getNumberData();
-    });
-
-    app.get("/frequentNums", function(req, res) {
-        res.send(getMostRepeated());
-    });
-
-    app.get("/evenOdd", function(req, res) {
-        res.send(getEvenOddData());
-    });
-
-// Connects to the lottery website and parses the data when the server is started.
-    app.listen(port, function() {
-        console.log("Server started successfully");
-        console.log("Creating JSON file...");
-        createJsonFile();
-    });
-}
-
 // app.use(function(req, res, next) {
 //     res.header('Access-Control-Allow-Origin', '*');
 //     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 //     next();
 // });
 
-// app.get("/", function(req, res){
-//     getNumberData();
-// });
-//
-// app.get("/frequentNums", function(req, res) {
-//     res.send(getMostRepeated());
-// });
-//
-// app.get("/evenOdd", function(req, res) {
-//    res.send(getEvenOddData());
-// });
-//
-// // Connects to the lottery website and parses the data when the server is started.
-// app.listen(port, function() {
-//     console.log("Server started successfully");
-//     console.log("Creating JSON file...");
-//     createJsonFile();
-// });
+app.use(express.static('frontend/build'));
+
+app.get('*', (req, res) => {
+    req.sendFile(path.resolve(__dirname, 'frontend/build', 'index.html'));
+})
+
+app.get("/", function(req, res){
+    getNumberData();
+});
+
+app.get("/frequentNums", function(req, res) {
+    res.send(getMostRepeated());
+});
+
+app.get("/evenOdd", function(req, res) {
+   res.send(getEvenOddData());
+});
+
+// Connects to the lottery website and parses the data when the server is started.
+app.listen(port, function() {
+    console.log("Server started successfully");
+    console.log("Creating JSON file...");
+    createJsonFile();
+});
