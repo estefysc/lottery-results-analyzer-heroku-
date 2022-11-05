2ac6d1872ee897a26e2ed56b3823e5233ea2dd94
Estefania Sanchez <estefysc@gmail.com>
2022-11-05 14:13:32 -0400

    Making the navbar responsive

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -4,45 +4,53 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import "./Navigation.css";
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBars} from "@fortawesome/free-solid-svg-icons";
 import {faPlusMinus} from '@fortawesome/free-solid-svg-icons';
 import {LinkContainer} from 'react-router-bootstrap'
 
 function Navigation() {
     return (
         <>
-            <Navbar variant="dark" expand="md">
+            <Navbar variant="dark" expand="lg">
+                {/*<div className="container">*/}
                 <Navbar.Brand className="brand">
                     <FontAwesomeIcon icon={faPlusMinus} />
                 </Navbar.Brand>
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                            {/*<LinkContainer to="/">*/}
-                            {/*    <Nav.Link className="nav-link">*/}
-                            {/*        Home*/}
-                            {/*    </Nav.Link>*/}
-                            {/*</LinkContainer>*/}
-                            <LinkContainer to="about">
-                                <Nav.Link className="nav-link">
-                                    About
-                                </Nav.Link>
+                <Navbar.Toggle className="collapsed-nav" aria-controls="basic-navbar-nav">
+                        <span >
+                            <FontAwesomeIcon className="bars" icon={faBars} />
+                        </span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="responsive-navbar-nav" >
+                    <Nav className="me-auto">
+                        {/*<LinkContainer to="/">*/}
+                        {/*    <Nav.Link className="nav-link">*/}
+                        {/*        Home*/}
+                        {/*    </Nav.Link>*/}
+                        {/*</LinkContainer>*/}
+                        <LinkContainer to="about">
+                            <Nav.Link className="nav-link">
+                                About
+                            </Nav.Link>
+                        </LinkContainer>
+                        <NavDropdown className="drp-menu" title="Explore" id="navbarScrollingDropdown">
+                            <LinkContainer to="frequentNums">
+                                <NavDropdown.Item className="drp-item">
+                                    Frequent Numbers
+                                </NavDropdown.Item>
                             </LinkContainer>
-                            <NavDropdown className="drp-menu" title="Explore" id="navbarScrollingDropdown">
-                                <LinkContainer to="frequentNums">
-                                    <NavDropdown.Item className="drp-item">
-                                        Frequent Numbers
-                                    </NavDropdown.Item>
-                                </LinkContainer>
-                                <LinkContainer to="evenOdds">
-                                    <NavDropdown.Item className="drp-item">
-                                        Evens and Odds
-                                    </NavDropdown.Item>
-                                </LinkContainer>
-                                {/*<NavDropdown.Item className="drp-item">*/}
-                                {/*    Something else here*/}
-                                {/*</NavDropdown.Item>*/}
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
+                            <LinkContainer to="evenOdds">
+                                <NavDropdown.Item className="drp-item">
+                                    Evens and Odds
+                                </NavDropdown.Item>
+                            </LinkContainer>
+                            {/*<NavDropdown.Item className="drp-item">*/}
+                            {/*    Something else here*/}
+                            {/*</NavDropdown.Item>*/}
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+                {/*</div>*/}
             </Navbar>
         </>
     );
